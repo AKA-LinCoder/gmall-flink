e6b5bc9366058416447c893ed1ef16e16db7999d
lsaac <2423860442@qq.com>
2024-02-23 10:12:34 +0800

    first commit

---
+++ gmall-realtime/src/main/java/com/echo/app/dim/DimApp.java
@@ -0,0 +1,91 @@
+package com.echo.app.dim;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.echo.app.func.TableProcessFunction;
+import com.echo.bean.TableProcess;
+import com.echo.utils.MyKafkaUtil;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.BroadcastConnectedStream;
+import org.apache.flink.streaming.api.datastream.BroadcastStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+
+public class DimApp {
+
+    public static void main(String[] args) throws Exception {
+        //TODO 1,获取执行环境
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        environment.setParallelism(1);//生产环境设置为Kafka主题的分区数量
+        //1.1开启checkPoint,5分钟一次
+        environment.enableCheckpointing(5*60000L, CheckpointingMode.EXACTLY_ONCE);
+        environment.getCheckpointConfig().setCheckpointTimeout(10*60000L);
+        environment.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+        environment.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,5000L));
+        //1.2设置状态后端
+        environment.setStateBackend(new HashMapStateBackend());
+        environment.getCheckpointConfig().setCheckpointStorage("hdfs://hadoop102:8020/211126/ck");
+        System.setProperty("HADOOP_USER_NAME","atguigu");
+
+        //TODO 2,读取Kafka topic_db主题数据创建主流
+        String topic = "topic_db";
+        String groupId = "Dim_App_211126";
+        DataStreamSource<String> kafkaDS = environment.addSource(MyKafkaUtil.getFlinkKafkaConsumer(topic, groupId));
+
+        //TODO 3，过滤非JSON数据以及保留新增，变化以及初始化数据为json
+        SingleOutputStreamOperator<JSONObject> filterJsonObjectDS = kafkaDS.flatMap(new FlatMapFunction<String, JSONObject>() {
+            @Override
+            public void flatMap(String s, Collector<JSONObject> collector) throws Exception {
+                try {
+                    //将数据转为json
+                    JSONObject jsonObject = JSON.parseObject(s);
+                    String type = jsonObject.getString("type");
+                    //保留新增,变化，以及初始化数据
+                    if ("insert".equals(type) || "update".equals(type) || "bootstrap-insert".equals(type)) {
+                        collector.collect(jsonObject);
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("发现脏数据" + s);
+                }
+
+            }
+        });
+        //TODO 4，使用flinkCDC 读取mysql配置信息表创建配置流
+        MySqlSource<String> sqlSource = MySqlSource.<String>builder()
+                .hostname("192.168.10.102")
+                .port(3306)
+                .username("root")
+                .password("000000")
+                .databaseList("gmall-config")
+                .tableList("gmall-config.table_process")
+                .scanNewlyAddedTableEnabled(true) // 开启支持新增表
+                .deserializer(new JsonDebeziumDeserializationSchema())
+                .startupOptions(StartupOptions.initial())
+                .build();
+        DataStreamSource<String> streamSource = environment.fromSource(sqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source");
+        //TODO 5，将配置流处理为广播流
+        MapStateDescriptor<String, TableProcess> mapStateDescriptor = new MapStateDescriptor<String, TableProcess>("map-state",String.class, TableProcess.class);
+        BroadcastStream<String> broadcastStream = streamSource.broadcast(mapStateDescriptor);
+        //TODO 6，连接主流与广播流
+        BroadcastConnectedStream<JSONObject, String> connectedStream = filterJsonObjectDS.connect(broadcastStream);
+        
+        //TODO 7，处理连接流，根据配置信息处理主流数据
+        SingleOutputStreamOperator<JSONObject> dimDs = connectedStream.process(new TableProcessFunction(mapStateDescriptor));
+        //TODO 8，将数据写出到Phoenix
+        dimDs.print(">>>>>>>>");
+        //TODO 9 启动任务
+        environment.execute("dimApp");
+    }
+}
---
+++ gmall-realtime/src/main/java/com/echo/app/func/TableProcessFunction.java
@@ -0,0 +1,171 @@
+package com.echo.app.func;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.echo.bean.TableProcess;
+import com.echo.common.GmallConfig;
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
+
+public class TableProcessFunction extends BroadcastProcessFunction<JSONObject, String, JSONObject> {
+
+
+    private Connection connection;
+
+    private MapStateDescriptor<String,TableProcess> mapStateDescriptor;
+
+    public TableProcessFunction(MapStateDescriptor<String, TableProcess> mapStateDescriptor) {
+        this.mapStateDescriptor = mapStateDescriptor;
+    }
+
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        connection = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
+    }
+
+
+
+
+    @Override
+    public void processBroadcastElement(String s, BroadcastProcessFunction<JSONObject, String, JSONObject>.Context context, Collector<JSONObject> collector) throws Exception {
+        //处理广播流
+        //1 获取并解析数据，方便主流操作
+        JSONObject jsonObject = JSON.parseObject(s);
+        TableProcess tableProcess = JSON.parseObject(jsonObject.getString("after"), TableProcess.class);
+        //2 校验表是否存在，如果不存在则需要在phoenix中创建表
+        checkTable(tableProcess.getSinkTable(),tableProcess.getSinkColumns(),tableProcess.getSinkPk(),tableProcess.getSinkExtend());
+        //3 写入状态，广播出去
+        BroadcastState<String, TableProcess> broadcastState = context.getBroadcastState(mapStateDescriptor);
+        broadcastState.put(tableProcess.getSourceTable(),tableProcess);
+
+    }
+
+    /**
+     * 校验并建表
+     * @param sinkTable
+     * @param sinkColumns
+     * @param sinkPk
+     * @param sinkExtend
+     */
+    private void checkTable(String sinkTable, String sinkColumns, String sinkPk, String sinkExtend) {
+        //处理特殊字段
+        if(sinkPk==null||"".equals(sinkPk)){
+            sinkPk = "id";
+        }
+
+        if(sinkExtend==null){
+            sinkExtend="";
+        }
+        PreparedStatement preparedStatement = null;
+
+     try {
+         //拼接sql
+         StringBuffer createTableIfSql = new StringBuffer("create table if not exists ")
+                 .append(GmallConfig.HBASE_SCHEMA)
+                 .append(".")
+                 .append(sinkTable)
+                 .append("(");
+         String[] split = sinkColumns.split(",");
+         for (int i = 0;i<split.length;i++){
+             //取出字段
+             String column = split[i];
+             //判断是否是主键
+             if(sinkPk.equals(column)){
+
+                 createTableIfSql.append(column).append(" varchar primary key");
+
+             }else {
+                 createTableIfSql.append(column).append(" varchar");
+             }
+             //判断是否是最后一个字段
+             if(i<split.length-1){
+                 createTableIfSql.append(",");
+             }
+         }
+         createTableIfSql.append(")").append(sinkExtend);
+         //编译sql
+         System.out.println(createTableIfSql);
+         preparedStatement = connection.prepareStatement(createTableIfSql.toString());
+         //执行sql,建表
+         preparedStatement.execute();
+
+     }catch (SQLException sqlException){
+         throw new  RuntimeException("建表失败" + sinkTable);
+     } finally {
+
+         //释放资源
+         if(preparedStatement!=null){
+
+             try {
+                 //释放资源
+                 preparedStatement.close();
+             }catch (SQLException e){
+                 e.printStackTrace();
+             }
+         }
+     }
+
+
+    }
+
+    @Override
+    public void processElement(JSONObject jsonObject, BroadcastProcessFunction<JSONObject, String, JSONObject>.ReadOnlyContext readOnlyContext, Collector<JSONObject> collector) throws Exception {
+        //处理主流数据
+        //1 获取广播的配置数据
+        ReadOnlyBroadcastState<String, TableProcess> broadcastState = readOnlyContext.getBroadcastState(mapStateDescriptor);
+        String table = jsonObject.getString("table");
+        TableProcess tableProcess = broadcastState.get(table);
+
+        if(tableProcess!=null){
+            //2 过滤字段
+            filterColumn(jsonObject.getJSONObject("data"),tableProcess.getSinkColumns());
+            //3 补充sinkTable字段输出
+            jsonObject.put("sinkTable",tableProcess.getSinkTable());
+            collector.collect(jsonObject);
+
+        } else{
+
+            System.out.println("找不到对应的key"+table);
+        }
+
+
+
+    }
+
+    /**
+     * 过滤数据
+     * @param data {"id":13,"tm_name":"dasdas","logo":"xxx"}
+     * @param sinkColumns "id,tm_name"
+     */
+    private void filterColumn(JSONObject data, String sinkColumns) {
+
+
+        String[] columns = sinkColumns.split(",");
+        List<String> columnList = Arrays.asList(columns);
+
+
+        Set<Map.Entry<String, Object>> entries = data.entrySet();
+        entries.removeIf(next -> !columnList.contains(next.getKey()));
+//        Iterator<Map.Entry<String, Object>> iterator = entries.iterator();
+//        while ((iterator.hasNext())){
+//            Map.Entry<String, Object> next = iterator.next();
+//            if(!sinkColumns.contains(next.getKey())){
+//                iterator.remove();
+//            }
+//        }
+
+    }
+
+
+}
