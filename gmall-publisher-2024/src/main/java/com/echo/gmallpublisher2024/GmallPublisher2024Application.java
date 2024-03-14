2785cfcd1576108d4da0c94ac00d4a142c0f1ec5
lsaac <2423860442@qq.com>
2024-03-14 15:26:07 +0800

    待测试

--- gmall-publisher-2024/src/main/java/com/echo/gmallpublisher2024/GmallPublisher2024Application.java
+++ gmall-publisher-2024/src/main/java/com/echo/gmallpublisher2024/GmallPublisher2024Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.echo.gmallpublisher2024.mapper")
+@MapperScan("com.echo.gmallpublisher2024.mapper")
 public class GmallPublisher2024Application {
 
     public static void main(String[] args) {
--- gmall-publisher-2024/src/main/java/com/echo/gmallpublisher2024/service/impl/UvServiceImpl.java
+++ gmall-publisher-2024/src/main/java/com/echo/gmallpublisher2024/service/impl/UvServiceImpl.java
@@ -3,11 +3,14 @@ package com.echo.gmallpublisher2024.service.impl;
 import com.echo.gmallpublisher2024.mapper.UvMapper;
 import com.echo.gmallpublisher2024.service.UvService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class UvServiceImpl implements UvService {
 
     @Autowired
@@ -16,12 +19,12 @@ public class UvServiceImpl implements UvService {
     @Override
     public Map getUvByCh(int date) {
         //创建hashmap用户存放结果数据
-        HashMap<String, Long> resultMap = new HashMap<>();
+        HashMap<String, BigInteger> resultMap = new HashMap<>();
         //查询clickhouse获取数据
         List<Map> mapList = uvMapper.selectUvByCh(date);
         //遍历集合，取出渠道和日活数据放入结果集
         for (Map map : mapList) {
-            resultMap.put((String) map.get("ch"),(Long) map.get("uv"));
+            resultMap.put((String) map.get("ch"),(BigInteger) map.get("uv"));
         }
         return resultMap;
     }
