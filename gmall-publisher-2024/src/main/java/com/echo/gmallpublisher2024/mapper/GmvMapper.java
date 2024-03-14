819ab1ac1aaa1c596bcd7aea099bb4fd494d1766
Estim <2423860442@qq.com>
2024-03-14 11:16:12 +0800

    修改bug,测试通过

--- gmall-publisher-2024/src/main/java/com/echo/gmallpublisher2024/mapper/GmvMapper.java
+++ gmall-publisher-2024/src/main/java/com/echo/gmallpublisher2024/mapper/GmvMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Select;
 
 public interface GmvMapper {
     //查询clickhouse
-    @Select("select sum(order_amount) from dws_trade_province_order_window where toYYYYMMDD(stt)=#{date}})")
+    @Select("select sum(cart_add_uu_ct) from dws_trade_cart_add_uu_window where toYYYYMMDD(stt)=#{date}")
+//    @Select("select sum(order_amount) from dws_trade_province_order_window where toYYYYMMDD(stt)=#{date}})")
     Double selectGMV(int date);
 }
