518d7528c3b0434f75d9ed18bdd9d654b20db5a7
lsaac <2423860442@qq.com>
2024-04-16 10:48:15 +0800

    准备测试socket1

---
+++ flink-tutorial-117/src/test/java/com/echo/flinktutorial1_17/FlinkTutorial117ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echo.flinktutorial1_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkTutorial117ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
